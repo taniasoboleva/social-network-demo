00cb45f3e14e41dbd4d7d688e15d2139caa42307
Soboleva <tatjana.soboleva@accenture.com>
2021-01-07 17:53:29 +0200

    added in App.js for Browserouter basename

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ let AppContainer = compose(
     connect(mapStateToProps, {initializeApp}))(App);
 
 const SamuraiJSApp = (props) => {
-    return <BrowserRouter>
+    return <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <AppContainer/>
         </Provider>
